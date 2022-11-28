0c16487d756d1b43f0729f4940d3b525cf4748a6
Helliozz <helliozz228@gmail.com>
2022-11-28 09:57:02 +0300

    completed homework 3.4

--- hometask34/src/main/kotlin/Main.kt
+++ hometask34/src/main/kotlin/Main.kt
@@ -1,7 +1,40 @@
-fun main(args: Array<String>) {
-    println("Hello World!")
+fun main() {
+    println(changer())
+    println(ave())
+}
 
-    // Try adding program arguments via Run/Debug configuration.
-    // Learn more about running applications: https://www.jetbrains.com/help/idea/running-applications.html.
-    println("Program arguments: ${args.joinToString()}")
+/**
+ * 1) Поменять максимальный и минимальный элемент в котлиновской коллекции
+ */
+fun changer(): ArrayList<Int> {
+    val arr: ArrayList<Int> = arrayListOf()
+    var max=0
+    var min=0
+    println("Enter size")
+    val size = readLine()?.toIntOrNull()
+    for(i in 0 until size!!){
+        arr.add((0..100).random())
+        if(arr[i]>arr[max]) max=i
+        if(arr[i]<arr[min]) min=i
+    }
+    println(arr)
+    val a = arr[max]
+    arr[max]=arr[min]
+    arr[min]=a
+    return arr
+}
+
+/**
+ * 2) заменить все значения массива на среднее арифметическое его элементов (использовать ArrayList)
+ */
+fun ave(): ArrayList<Int> {
+    val arr: ArrayList<Int> = arrayListOf()
+    println("Enter size")
+    val size = readLine()?.toIntOrNull()
+    for(i in 0 until size!!) arr.add((0..100).random())
+    println(arr)
+    val ave=arr.average().toInt()
+    for(i in 0 until size) arr[i]=ave
+    return arr
+
 }
